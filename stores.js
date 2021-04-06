6e3a066a44408b1e1d03c9289d47f3f76e2af568
achook <karol@kucza.xyz>
2021-04-06 19:57:11 +0200

    Dodanie map

--- stores.js
+++ stores.js
@@ -2,7 +2,21 @@ let map
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    center: { lat: 52, lng: 19 },
+    zoom: 5,
   })
+
+  for (let i = 0; i < 20; i++) {
+    const lat = 50.0 + (4.0 * Math.random())
+    const lng = 15.0 + (8.0 * Math.random())
+    const position = { lat: lat, lng: lng }
+
+    new google.maps.Marker({
+        position: position,
+        map: map,
+        title: "Sklep Malwa",
+        label: i.toString()
+    })
+  }
+
 }
